d9d99cdfe5eab20116d84bb2847f16e733884f10
pmapelli <pmapelli@hotmail.com>
2019-10-03 18:25:18 -0300

    feat: Add Styled-Component

---
+++ src/components/ChatList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Container } from "./style";
+
+export default class ChatList extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>Teste</h1>
+        <p>Texto teste</p>
+      </Container>
+    );
+  }
+}
